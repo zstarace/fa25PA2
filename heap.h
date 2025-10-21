395d48d049438aa6b37146ae04ad94aa7c6c628a
Zoe Starace <zstarace1462@sdsu.edu>
2025-10-21 14:38:34 -0700

    started working on heap.h havent tested it

--- heap.h
+++ heap.h
@@ -16,20 +16,46 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        if (size>= 64) {
+            cerr << "Heap Overflow\n"; //character error message
+            return;
+        }
+        int pos = size ++;
+        data[pos] = idx;
+        upheap(pos, weightArr);
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        // placeholder
+        if (size == 0) {
+            cerr << "Heap Underflow\n";
+            return -1;
+        }
+        int minIdx = data[0];
+        downheap(0,weightArr);
+        return minIdx;
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+        while (pos>0) {
+            int parent = (pos-1)/2;
+
+            if (weightArr[data[pos]] < weightArr[data[parent]] ||(weightArr[data[pos]] == weightArr[data[parent]] && data[pos] < data[parent])) {
+                swap(data[pos], data[parent]);
+                pos = parent;
+            }
+            else {
+                break;
+            }
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+
     }
 };
 
