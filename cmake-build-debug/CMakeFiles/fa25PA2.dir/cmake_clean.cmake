f5d3ecf94ad1c460ed2ecc3f6616c056e9b48b82
Zoe Starace <zstarace1462@sdsu.edu>
2025-10-21 14:38:44 -0700

    started working on heap.h havent tested it

---
+++ cmake-build-debug/CMakeFiles/fa25PA2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fa25PA2.dir/main.cpp.o"
+  "CMakeFiles/fa25PA2.dir/main.cpp.o.d"
+  "fa25PA2"
+  "fa25PA2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fa25PA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/fa25PA2.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for fa25PA2.
+# This may be replaced when dependencies are built.
